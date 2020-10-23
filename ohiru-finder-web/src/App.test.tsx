8a15d3493a0137577a10342c00c9a41e1aec018b
iumehara <iumehara10@gmail.com>
2020-10-23 16:43:24 +0900

    Add react web app

---
+++ ohiru-finder-web/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText('Ohiru Finder');
+  expect(linkElement).toBeInTheDocument();
+});
